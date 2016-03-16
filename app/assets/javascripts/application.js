f81f6437fac73ecca9e791dc2b73840b501489b3
LylyG <lgalarza@spoonflower.com>
2016-03-16 01:50:03 -0400

    Disable button after one click is complete.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,35 @@
 //= require jquery_ujs
 //= require d3
 //= require_tree .
+
+function disableButton() {
+  var button  = document.getElementsByClassName("actions");
+  button.lastElementChild.disabled = 'true';
+};
+
+function hideRow() {
+  var row = document.getElementsByClassName("row");
+  row[row.length - 1].style.visibility = "hidden";
+};
+
+// function showLastRow() {
+//   var row = document.getElementsByClassName("row");
+//   var lastRow = row[row.length - 1].cloneNode(true);
+//   row.appendChild(lastRow);
+// };
+
+function showLastRow() {
+  var row = document.getElementsByClassName("row");
+  row[row.length - 1].style.visibility = "block";
+};
+
+function deleteRow () {
+  var deleteGrade = document.getElementsByClassName("row");
+};
+
+function smoothScrolling () {
+
+};
+
+
+window.onload=hideRow;
